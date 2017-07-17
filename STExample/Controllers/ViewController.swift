b2daa7d8961faa1dd4c0991721034a3219019166
Enric Sánchez <esanchez@mdut3s-MacBook-Pro-6.local>
2017-07-17 14:08:00 +0200

    remove log

--- STExample/Controllers/ViewController.swift
+++ STExample/Controllers/ViewController.swift
@@ -20,7 +20,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        debugPrint("miau")
     }
 
     override func didReceiveMemoryWarning() {
