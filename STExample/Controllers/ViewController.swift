1e9275f437fcb63e55a2be3eab8ee828d42b6a38
Enric Sánchez <esanchez@mdut3s-MacBook-Pro-6.local>
2017-07-24 11:43:43 +0200

    test fail

--- STExample/Controllers/ViewController.swift
+++ STExample/Controllers/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
   
     override func viewDidLoad() {
         super.viewDidLoad()
-        lbTest.text = "ST Example"
+        lbTest.text = "TITLE_ES"
     }
 
     override func didReceiveMemoryWarning() {
